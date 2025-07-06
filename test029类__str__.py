0e8a35da57e5278cab2effda271f75c1ec447a57
muyang-666 <2870437965@qq.com>
2025-07-06 23:44:43 +0800

    2025.7.6

---
+++ test029类__str__.py
@@ -0,0 +1,23 @@
+class Person:
+    def __init__(self,name,weight):
+        self.name = name
+        self.weight = weight
+
+    def __str__(self):
+        return f"姓名: {self.name},体重: {self.weight}kg"
+
+    def run(self):
+        print(f'{self.name} 跑步 5 km, 体重减少了')
+        self.weight -= 0.5
+
+    def eat(self):
+        print(f'{self.name} 大餐一顿, 体重增加了')
+        self.weight += 1
+
+        
+xm = Person('小明',75.0)
+print(xm)
+xm.run()
+print(xm)
+xm.eat()
+print(xm)
